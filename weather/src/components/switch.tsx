d96f123fec9d26fa9c335db76f0cdc3624e5ae7a
codercsx <83553175@qq.com>
2025-07-17 21:42:42 +0800

    feat: weather删除模板默认内容

---
+++ weather/src/components/switch.tsx
@@ -0,0 +1,6 @@
+export default function Switch() {
+    return (
+        <div className="switch absolute top-0">
+        </div>
+    )
+}
--- weather/src/main.tsx
+++ weather/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import './assets/style/index.less'
 import 'virtual:uno.css'
 
 
